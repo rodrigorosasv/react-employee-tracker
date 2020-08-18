5ca155a5522d39be50bd2a6667957bdbd6d69720
Rodrigo Rosas <rodrigo.rosasv@gmail.com>
2020-08-18 18:22:47 -0500

    Search by name working

--- src/components/EmployeeContainer.js
+++ src/components/EmployeeContainer.js
@@ -8,48 +8,87 @@ import Footer from "./Footer";
 import EmployeeDetail from "./EmployeeDetail";
 import API from "../utils/API";
 import Titles from "./Titles";
+import Row from "./Row";
 
-export default class PersonList extends React.Component {
+// function filterItems(query, employees) {
+//   return employees.filter(function(el) {
+//       return el.toString().toLowerCase().indexOf(query.toString().toLowerCase()) > -1;
+//   })
+// }
+
+export default class EmployeeContainter extends React.Component {
   state = {
-    employees: []
+    filter:"",
+    employees: [],
+    filterEmployee: []
   }
 
   componentDidMount() {
-    axios.get(`https://randomuser.me/api/?results=100&nat=us`)
+    axios.get(`https://randomuser.me/api/?results=200&inc=email,name,dob,picture,phone&nat=us`)
       .then(res => {
         const employees = res.data.results;
-        console.log(employees);
-        this.setState({ employees });
+        const filterEmployee= res.data.results;
+        // console.log(employees);
+        this.setState({ employees,filterEmployee });
       })
   }
 
-  handleFormSubmit = event => {
-    event.preventDefault();
+  handleChange = event => {
+    // const search = event.target.value.toLowerCase();
+    // const name = event.target.name;
+    // console.log("Search "+search);
+    // console.log("Name "+name);
 
+    this.setState({ filter: event.target.value });
+    // const filter= event.target.value.toLowerCase();
+    //console.log("Filter "+filter)
+    // const allEmployees=this.state.employees;
+    // console.log("Allemployees "+allEmployees);
   };
 
   render() {
+    const { filter, employees } = this.state;
+    console.log("Render filter "+filter);
+    // const newFilter=filter.toLocaleLowerCase();
+    // console.log("New filter "+newFilter);
+    // var filtered = employees.filter(c=> Object.keys(c).some(key=>c[key].includes(filter)));
+    // console.log(filtered);
+
+    // const results = employees.filter((obj)=>{
+    //   return Object.keys(obj).reduce((acc, curr)=>{
+    //         return acc || obj[curr].toLowerCase().includes(filter);
+    //   }, false);
+    // }); 
+
+    // console.log(results);
+
     return (
       <div>
         <Navbar />
-        
         <Container>
-        <SearchForm
-                value={this.state.search}
-                handleInputChange={this.handleInputChange}
-                handleFormSubmit={this.handleFormSubmit}
-        />
+          <div>Search here: <input value={filter} onChange={this.handleChange} /></div>
+        
         <Titles />
-          {this.state.employees.map(employees => (
-          <EmployeeDetail
-              id={employees.gender}
-              name={employees.name.first+" "+employees.name.last}
-              email={employees.email}
-              phone={employees.phone}
-              image={employees.picture.large}
-              dob={employees.dob.date}
-          />
-          ))}
+        {employees.filter(name=>name.name.first.includes(filter)).map(filteredEmployee => (
+          <div className="row">
+                  <div className="col-2">
+                    <img alt={filteredEmployee.name.first} src={filteredEmployee.picture.large} />
+                  </div>
+                  <div className="col-2">
+                    {filteredEmployee.name.first} {filteredEmployee.name.last}
+                  </div>
+                  <div className="col-2">
+                    {filteredEmployee.phone}
+                  </div>
+                  <div className="col-2">
+                    {filteredEmployee.email}
+                  </div>
+                  <div className="col-2">
+                    {filteredEmployee.dob.date.substring(0,10)}
+                  </div>
+            {filteredEmployee.email}
+          </div>
+        ))}
         </Container>
         <Footer />
       </div>
--- src/utils/API.js
+++ src/utils/API.js
@@ -4,6 +4,6 @@ import axios from "axios";
 
 export default {
   search: function() {
-    return axios.get("https://randomuser.me/api/?results=10");
+    return axios.get("https://randomuser.me/api/?results=10&inc=email,name,dob,picture,phone&nat=us");
   }
 };
