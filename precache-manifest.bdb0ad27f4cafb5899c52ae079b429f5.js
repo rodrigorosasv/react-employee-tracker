0c3c7de2e5adcff4360db6522b0ea2bf68117fc8
Rodrigo Rosas <rodrigo.rosasv@gmail.com>
2020-08-18 19:19:45 -0500

    Updates

---
+++ precache-manifest.bdb0ad27f4cafb5899c52ae079b429f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b0b5fbb29f78eef7862398bb23a8ce2",
+    "url": "/react-employee-tracker/index.html"
+  },
+  {
+    "revision": "3e3c5d22ecd39c6791b3",
+    "url": "/react-employee-tracker/static/js/2.bbc4e55e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-tracker/static/js/2.bbc4e55e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a99824feea86f37625d5",
+    "url": "/react-employee-tracker/static/js/main.d9209c04.chunk.js"
+  },
+  {
+    "revision": "c58193bca35bbd54f2cd",
+    "url": "/react-employee-tracker/static/js/runtime-main.0718a10a.js"
+  }
+]);
